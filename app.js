b52c7419d5ff8ab5862258c13b502e6899fa4310
Koby-Johnson <knjohnson@alarm.com>
2021-03-15 09:30:39 -0400

    Adding module 2 parts

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const BASE_URL = "https://jsonplace-univclone.herokuapp.com";
 // Can build str like this... `${ BASE_URL }/users`
 
+function fetchData(url) {
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+}
+
 function fetchUsers() {
-  return fetch(`${BASE_URL}/users`)
-    .then( response => response.json())
-    .catch( error => console.log(error));
+  return fetchData(`${BASE_URL}/users`);
 }
 
 function renderUser(user) {
@@ -32,6 +36,51 @@ function renderUserList(userList) {
   });
 }
 
+/* get an album list, or an array of albums */
+function fetchUserAlbumList(userId) {
+  return fetchData(
+    `${BASE_URL}/users/${userId}/albums?_expand=user&_embed=photos`
+  );
+}
+
+/* render a single album */
+function renderAlbum(album) {
+  const albumCard = $(`
+    <div class="album-card">
+      <header>
+        <h3>${album.title}, by ${album.user.username} </h3>
+      </header>
+      <section class="photo-list">
+      </section>
+    </div>
+  `);
+  for (let photo of album.photos) {
+    albumCard.find(".photo-list").append(renderPhoto(photo));
+  }
+  return albumCard;
+}
+
+/* render a single photo */
+function renderPhoto(photo) {
+  return `
+    <div class="photo-card">
+      <a href="${photo.url}" target="_blank">
+        <img src="${photo.thumbnailUrl}">
+        <figure>${photo.title}</figure>
+      </a>
+    </div>
+  `;
+}
+
+/* render an array of albums */
+function renderAlbumList(albumList) {
+  $("#app section.active").removeClass("active");
+  $("#album-list").addClass("active").empty();
+  albumList.forEach((el) => {
+    $("#album-list").append(renderAlbum(el));
+  });
+}
+
 function bootstrap() {
   fetchUsers().then(renderUserList);
 }
@@ -45,7 +94,8 @@ $("#user-list").on("click", ".user-card .load-posts", function () {
 });
 
 $("#user-list").on("click", ".user-card .load-albums", function () {
-  console.log($(this).closest(".user-card").data("user"));
+  const userObj = $(this).closest(".user-card").data("user");
+  fetchUserAlbumList(userObj.id).then(renderAlbumList);
   // load albums for this user
   // render albums for this user
 });
