a1b5f901651d34908adf6739d504caeb0e9e6af8
Koby-Johnson <knjohnson@alarm.com>
2021-03-15 13:03:38 -0400

    Adding module 3 requirements

--- app.js
+++ app.js
@@ -81,14 +81,98 @@ function renderAlbumList(albumList) {
   });
 }
 
+function fetchUserPosts(userId) {
+  return fetchData(`${BASE_URL}/users/${userId}/posts?_expand=user`);
+}
+
+function fetchPostComments(postId) {
+  return fetchData(`${BASE_URL}/posts/${postId}/comments`);
+}
+
+function renderPost(post) {
+  const postJQ = $(`
+    <div class="post-card">
+      <header>
+        <h3>${post.title}</h3>
+        <h3>--- ${post.user.username}</h3>
+      </header>
+      <p>${post.body}</p>
+      <footer>
+        <div class="comment-list"></div>
+        <a href="#" class="toggle-comments">(<span class="verb">show</span> comments)</a>
+      </footer>
+    </div>
+  `);
+  postJQ.data("post", post);
+  return postJQ;
+}
+
+function renderPostList(postList) {
+  $("#app section.active").removeClass("active");
+  $("#post-list").addClass("active").empty();
+  postList.forEach((el) => {
+    $("#post-list").append(renderPost(el));
+  });
+}
+
+function setCommentsOnPost(post) {
+  // post.comments might be undefined, or an []
+  // if undefined, fetch them then set the result
+  // if defined, return a rejected promise
+  // if we already have comments, don't fetch them again
+  if (post.comments) {
+    // #1: Something goes here
+    return Promise.reject(null);
+  }
+
+  // fetch, upgrade the post object, then return it
+  return fetchPostComments(post.id).then(function (comments) {
+    post.comments = comments;
+    return post;
+    // #2: Something goes here
+  });
+}
+
+function toggleComments(postCardElement) {
+  const footerElement = postCardElement.find("footer");
+
+  if (footerElement.hasClass("comments-open")) {
+    footerElement.removeClass("comments-open");
+    footerElement.find(".verb").text("show");
+  } else {
+    footerElement.addClass("comments-open");
+    footerElement.find(".verb").text("hide");
+  }
+}
+
 function bootstrap() {
   fetchUsers().then(renderUserList);
 }
 
 bootstrap();
 
+$("#post-list").on("click", ".post-card .toggle-comments", function () {
+  const postCardElement = $(this).closest(".post-card");
+  const post = postCardElement.data("post");
+
+  setCommentsOnPost(post)
+    .then(function (post) {
+      console.log("building comments for the first time...", post);
+      postCardElement.find('.comment-list').empty()
+      post.comments.forEach( comment => {
+        postCardElement.find('.comment-list').append(`<h3>email: ${comment.email}, post: ${comment.body}</h3>`)
+      })
+      toggleComments(postCardElement)
+    })
+    .catch(function () {
+      console.log("comments previously existed, only toggling...", post);
+      toggleComments(postCardElement)
+    });
+});
+
 $("#user-list").on("click", ".user-card .load-posts", function () {
-  console.log($(this).closest(".user-card").data("user"));
+  const userObj = $(this).closest(".user-card").data("user");
+  fetchUserPosts(userObj.id).then(renderPostList);
   // load posts for this user
   // render posts for this user
 });
